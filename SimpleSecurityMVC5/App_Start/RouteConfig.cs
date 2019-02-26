a81673d603bba7193476d2fbe906d7a574586e46
Jeremy Page <jeremy.page@trafford.gov.uk>
2019-02-26 09:44:42 +0000

    Tidy solutions
    Promote SimpleSecurityMVC5 from sub-solution

--- SimpleSecurityMVC5/App_Start/RouteConfig.cs
+++ SimpleSecurityMVC5/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace SimpleSecurityMVC5
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional }
+                defaults: new { action = "Index", controller = "Home", id = UrlParameter.Optional },
+                namespaces: new string[] { "SimpleSecurityMVC5.Controllers" }
             );
         }
     }
